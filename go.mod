c5cd6d779bc68556da3844211389b142ee178cdd
thisisaaronland <devnull@localhost>
2019-06-17 09:29:26 -0700

    wire in SES stuff

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/aaronland/go-mailinglist
 
 require (
 	github.com/aaronland/gomail v2.0.1+incompatible
+	github.com/aws/aws-sdk-go v1.20.1
+	github.com/go-gomail/gomail v0.0.0-20160411212932-81ebce5c23df
 	github.com/whosonfirst/algnhsa v0.1.0
+	github.com/whosonfirst/go-whosonfirst-aws v0.1.1
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 )
---
+++ sender/ses.go
@@ -0,0 +1,108 @@
+package sender
+
+// https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/ses-example-send-email.html
+// https://docs.aws.amazon.com/ses/latest/DeveloperGuide/verify-email-addresses-procedure.html
+
+// https://docs.aws.amazon.com/sdk-for-go/api/service/ses/#SES.VerifyEmailIdentity
+// https://docs.aws.amazon.com/sdk-for-go/api/service/ses/#SES.WaitUntilIdentityExists
+// https://docs.aws.amazon.com/sdk-for-go/api/service/ses/#SES.ListIdentities
+// https://docs.aws.amazon.com/sdk-for-go/api/service/ses/#SES.DeleteIdentity
+
+// https://docs.aws.amazon.com/ses/latest/DeveloperGuide/send-personalized-email-api.html
+// https://docs.aws.amazon.com/ses/latest/APIReference/API_CreateCustomVerificationEmailTemplate.html
+// https://docs.aws.amazon.com/sdk-for-go/api/service/ses/#SES.CreateCustomVerificationEmailTemplate
+
+// https://us-west-2.console.aws.amazon.com/ses/home?region=us-west-2#smtp-settings:
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"github.com/aws/aws-sdk-go/service/ses"
+	"github.com/go-gomail/gomail"
+	"github.com/whosonfirst/go-whosonfirst-aws/session"
+	"io"
+	_ "log"
+)
+
+type SESSender struct {
+	gomail.Sender
+	service *ses.SES
+}
+
+func NewSESSender(dsn string) (gomail.Sender, error) {
+
+	sess, err := session.NewSessionWithDSN(dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	svc := ses.New(sess)
+
+	s := SESSender{
+		service: svc,
+	}
+
+	// https://docs.aws.amazon.com/sdk-for-go/api/service/ses/#GetSendQuotaOutput
+
+	return &s, nil
+}
+
+func (s *SESSender) Send(from string, to []string, msg io.WriterTo) error {
+
+	var buf bytes.Buffer
+	wr := bufio.NewWriter(&buf)
+
+	_, err := msg.WriteTo(wr)
+
+	if err != nil {
+		return err
+	}
+
+	wr.Flush()
+
+	raw_msg := &ses.RawMessage{
+		Data: buf.Bytes(),
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	for _, recipient := range to {
+
+		err := s.sendMessage(ctx, from, recipient, raw_msg)
+
+		// maybe check err here and sometimes continue ?
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *SESSender) sendMessage(ctx context.Context, sender string, recipient string, msg *ses.RawMessage) error {
+
+	// throttle send here... (see quota stuff above)
+
+	select {
+	case <-ctx.Done():
+		return nil
+	default:
+		// pass
+	}
+
+	req := &ses.SendRawEmailInput{
+		RawMessage: msg,
+	}
+
+	_, err := s.service.SendRawEmailWithContext(ctx, req)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
