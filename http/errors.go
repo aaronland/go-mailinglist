8835e3a2c99c4c109e1b12e58b6b11d69e61af87
thisisaaronland <devnull@localhost>
2019-10-17 22:26:12 -0700

    start listing errors

--- http/errors.go
+++ http/errors.go
@@ -1,1 +1,24 @@
 package http
+
+// params sanitize error
+// params missing invitation code
+// params missing address
+
+// email parse error
+
+// invitation not found
+// invitation unavailable
+// invitation accept error
+// invitation update (db) error
+
+// subscription already exists <-- how to not leak data?
+// subscription create error
+// subscription add (to db) error
+
+// confirmation create error
+// confirmation add (to db) error
+
+// events add log error
+
+// message new message error
+// message send error
