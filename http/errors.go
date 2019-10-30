58b87768b51ee44b17296a8da396004419654748
thisisaaronland <devnull@localhost>
2019-10-30 07:55:12 -0700

    chip away at application error codes/messages

--- http/errors.go
+++ http/errors.go
@@ -5,26 +5,102 @@ import (
 	"log"
 )
 
+// -1 (house keeping)
+
+const E_UNKNOWN_ERROR int = -1
+const E_UNKNOWN_ERROR_MESSAGE = "Unknown error"
+
 // 01 - 99 (feature flags)
 
-const DISABLED_SUBSCRIBE int = 1
-const DISABLED_UNSUBSCRIBE int = 2
-const DISABLED_CONFIRM int = 3
-const DISABLED_INVITE int = 4
+const E_DISABLED_SUBSCRIBE int = 1
+const E_DISABLED_UNSUBSCRIBE int = 2
+const E_DISABLED_CONFIRM int = 3
+const E_DISABLED_INVITE int = 4
 
-const DISABLED_SUBSCRIBE_MESSAGE string = "Subscriptions are not currently enabled."
-const DISABLED_UNSUBSCRIBE_MESSAGE string = "Unsubscribing is not currently enabled."
+const E_DISABLED_SUBSCRIBE_MESSAGE string = "Subscriptions are not currently enabled."
+const E_DISABLED_UNSUBSCRIBE_MESSAGE string = "Unsubscribing is not currently enabled."
+const E_DISABLED_CONFIRM_MESSAGE string = "Confirmations are not currently enabled."
+const E_DISABLED_INVITE_MESSAGE string = "Invites are not currently enabled."
 
-var errors_map = map[int]string {
-	-1:                   "Unknown error",
-	DISABLED_SUBSCRIBE:   DISABLED_SUBSCRIBE_MESSAGE,
-	DISABLED_UNSUBSCRIBE: DISABLED_UNSUBSCRIBE_MESSAGE,
+// 100 - 199 (input validation)
+
+const E_INPUT_MISSING int = 100
+const E_INPUT_PARSE int = 101
+
+const E_INPUT_MISSING_MESSAGE string = "Missing parameter '%s'"
+const E_INPUT_PARSE_MESSAGE string = "Invalid parameter '%s'"
+
+// 200 - 299 (email)
+
+const E_EMAIL_INVALID int = 200
+const E_EMAIL_CREATE int = 201
+const E_EMAIL_SEND int = 202
+
+const E_EMAIL_INVALID_MESSAGE string = "Invalid email address '%s'."
+const E_EMAIL_CREATE_MESSAGE string = "Failed to create new email message."
+const E_EMAIL_SEND_MESSAGE string = "Failed to send email message."
+
+// 300 - 399 (subscriptions)
+
+const E_SUBSCRIPTION_EXISTS int = 300
+const E_SUBSCRIPTION_RETRIEVE int = 301
+const E_SUBSCRIPTION_NOTFOUND int = 302
+const E_SUBSCRIPTION_CREATE int = 303
+const E_SUBSCRIPTION_CONFIRM int = 304
+const E_SUBSCRIPTION_DISABLED int = 305
+const E_SUBSCRIPTION_ADD int = 306
+
+const E_SUBSCRIPTION_EXISTS_MESSAGE string = "Already subscribed." // how to not leak data...?
+const E_SUBSCRIPTION_RETRIEVE_MESSAGE string = "Failed to retrieve subscription."
+const E_SUBSCRIPTION_NOTFOUND_MESSAGE string = "Subscription not found."
+const E_SUBSCRIPTION_CREATE_MESSAGE string = "Failed to create new subscription."
+const E_SUBSCRIPTION_CONFIRM_MESSAGE string = "Failed to confirm subscription."
+const E_SUBSCRIPTION_DISABLED_MESSAGE string = "Subscription is disabled."
+const E_SUBSCRIPTION_ADD_MESSAGE string = "Failed to add subscription."
+
+// 400 - 499 (confirmations)
+
+const E_CONFIRMATION_RETRIEVE int = 400
+const E_CONFIRMATION_EXPIRED int = 401
+const E_CONFIRMATION_CREATE int = 402
+const E_CONFIRMATION_ADD int = 403
+const E_CONFIRMATION_INVALID int = 404
+
+const E_CONFIRMATION_RETRIEVE_MESSAGE string = "Failed to retrieve confirmation."
+const E_CONFIRMATION_EXPIRED_MESSAGE string = "Confirmation has expired."
+const E_CONFIRMATION_CREATE_MESSAGE string = "Failed to create confirmation."
+const E_CONFIRMATION_ADD_MESSAGE string = "Failed to add confirmation."
+const E_CONFIRMATION_INVALID_MESSAGE string = "Invalid confirmation."
+
+var errors_map = map[int]string{
+	E_UNKNOWN_ERROR:         E_UNKNOWN_ERROR_MESSAGE,
+	E_DISABLED_SUBSCRIBE:    E_DISABLED_SUBSCRIBE_MESSAGE,
+	E_DISABLED_UNSUBSCRIBE:  E_DISABLED_UNSUBSCRIBE_MESSAGE,
+	E_DISABLED_CONFIRM:      E_DISABLED_CONFIRM_MESSAGE,
+	E_DISABLED_INVITE:       E_DISABLED_INVITE_MESSAGE,
+	E_INPUT_MISSING:         E_INPUT_MISSING_MESSAGE,
+	E_INPUT_PARSE:           E_INPUT_PARSE_MESSAGE,
+	E_EMAIL_INVALID:         E_EMAIL_INVALID_MESSAGE,
+	E_EMAIL_CREATE:          E_EMAIL_CREATE_MESSAGE,
+	E_EMAIL_SEND:            E_EMAIL_SEND_MESSAGE,
+	E_SUBSCRIPTION_EXISTS:   E_SUBSCRIPTION_EXISTS_MESSAGE,
+	E_SUBSCRIPTION_RETRIEVE: E_SUBSCRIPTION_RETRIEVE_MESSAGE,
+	E_SUBSCRIPTION_NOTFOUND: E_SUBSCRIPTION_NOTFOUND_MESSAGE,
+	E_SUBSCRIPTION_CREATE:   E_SUBSCRIPTION_CREATE_MESSAGE,
+	E_SUBSCRIPTION_CONFIRM:  E_SUBSCRIPTION_CONFIRM_MESSAGE,
+	E_SUBSCRIPTION_DISABLED: E_SUBSCRIPTION_DISABLED_MESSAGE,
+	E_SUBSCRIPTION_ADD:      E_SUBSCRIPTION_ADD_MESSAGE,
+	E_CONFIRMATION_RETRIEVE: E_CONFIRMATION_RETRIEVE_MESSAGE,
+	E_CONFIRMATION_EXPIRED:  E_CONFIRMATION_EXPIRED_MESSAGE,
+	E_CONFIRMATION_CREATE:   E_CONFIRMATION_CREATE_MESSAGE,
+	E_CONFIRMATION_ADD:      E_CONFIRMATION_ADD_MESSAGE,
+	E_CONFIRMATION_INVALID:  E_CONFIRMATION_INVALID_MESSAGE,
 }
 
 type ApplicationError struct {
 	Code    int
 	Message string
-	Detail string
+	Detail  string
 }
 
 func (e *ApplicationError) Error() string {
@@ -41,7 +117,7 @@ func NewFeatureFlagError(code int) *ApplicationError {
 }
 
 func NewParamsError(code int, param string) *ApplicationError {
-	return NewApplicationError(code, param, nil)	
+	return NewApplicationError(code, param, nil)
 }
 
 func NewApplicationError(code int, detail string, err error) *ApplicationError {
@@ -58,40 +134,9 @@ func NewApplicationError(code int, detail string, err error) *ApplicationError {
 	return &ApplicationError{
 		Code:    code,
 		Message: msg,
-		Detail: detail,
+		Detail:  detail,
 	}
 }
-
-// 01 - 99 (feature flags)
-// subscription(s) disabled
-// unsubscribe disabled
-// invitation(s) disabled
-
-// 100 - 199
-// params sanitize error
-// params missing param
-
-// 200 - 299
-// email invalid address
-// email new message error
-// email send error
-
-// 300 - 399
-
-// subscription already exists <-- how to not leak data?
-// subscription retrieve error
-// subscription not found
-// subscription create (new) error
-// subscription confirm error
-// subscription disabled error
-// subscription add (to db) error
-
-// 400 - 499
-// confirmation retrieve error
-// confirmation expired error
-// confirmation create error
-// confirmation add (to db) error
-// confirmation invalid action error
 
 // 500 - 599
 // invitation retrieve / not found
