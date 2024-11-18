ac6568a02142222418737c42c8351b6a1a52868e
thisisaaronland <thisisaaronland@localhost>
2024-11-18 00:32:12 -0800

    keep blocking out methods

--- database/deliveries_docstore.go
+++ database/deliveries_docstore.go
@@ -54,8 +54,8 @@ func (db *DeliveriesDocstoreDatabase) AddDelivery(ctx context.Context, d *delive
 	return db.collection.Put(ctx, d)
 }
 
-func (db *DeliveriesDocstoreDatabase) ListDeliveries(ctx context.Context, db ListDeliveriesFunc) error {
-
+func (db *DeliveriesDocstoreDatabase) ListDeliveries(ctx context.Context, cb ListDeliveriesFunc) error {
+	return db.getDeliveriesWithCallback(ctx, cb)
 }
 
 func (db *DeliveriesDocstoreDatabase) GetDeliveryWithAddressAndMessageId(ctx context.Context, addr string, id string) (*delivery.Delivery, error) {
@@ -64,4 +64,31 @@ func (db *DeliveriesDocstoreDatabase) GetDeliveryWithAddressAndMessageId(ctx con
 
 func (db *DeliveriesDocstoreDatabase) Close() error {
 	return db.collection.Close()
+}
+
+func (db *DeliveriesDocstoreDatabase) getDeliveriesWithCallback(ctx context.Context, q *docstore.Query, cb ListDeliveriesFunc) error {
+
+	iter := q.Get(ctx)
+	defer iter.Stop()
+
+	for {
+
+		var d delivery.Delivery
+		err := iter.Next(ctx, &d)
+
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return fmt.Errorf("Failed to interate, %w", err)
+		} else {
+
+			err := cb(ctx, &d)
+
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
 }
