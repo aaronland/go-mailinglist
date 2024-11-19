adcae22044d89b9d33d0172136418f57e86f239e
thisisaaronland <thisisaaronland@localhost>
2024-11-18 18:03:23 -0800

    block out common methods

--- confirmations.go
+++ confirmations.go
@@ -1,1 +1,25 @@
 package mailinglist
+
+import (
+	"time"
+	"context"
+
+	"github.com/aaronland/go-mailinglist/v2/database"
+	"github.com/aaronland/go-mailinglist/v2/confirmation"
+)
+
+func AddConfirmation(ctx context.Context, db database.ConfirmationsDatabase, c *confirmation.Confirmation) (*confirmation.Confirmation, error) {
+
+	now := time.Now()
+	ts := now.Unix()
+
+	c.Created = ts
+
+	err := db.AddConfirmation(ctx, c)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
